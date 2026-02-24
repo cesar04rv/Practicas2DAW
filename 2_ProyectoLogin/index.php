2620f8ae7d06f4d1d085dfd9998bc95f41356ed9
César Rodríguez Vizcaíno <91700489+cesar04rv@users.noreply.github.com>
2026-02-24 17:05:59 +0100

    Add files via upload

---
+++ 2_ProyectoLogin/index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <title>Document</title>
+    <link rel="stylesheet" href="/styles/estilos.css">
+  </head>
+  <body>
+    <div class="container-all">
+      <div class="ctn-form">
+        <img src="/img/logo ghub.jfif" alt="" class="logo" />
+        <div class="campos">
+        <h1 class="title">Iniciar Sesión</h1>
+
+        <form action="">
+          <label for="email">Email</label>
+          <input type="text" id="email" name="email" required />
+          <label for="password">Contraseña</label>
+          <input type="password" id="password" name="password" required />
+
+          <input type="submit" value="Iniciar" />
+          </div>
+        </form>
+        <span class="text-footer"
+          >¿No tienes cuenta?
+          <a href="#">Regístrate aquí</a>
+        </span>
+
+      
+          </div>
+            <div class="ctn-text">
+          <div class="capa">
+            <h1 class="title-description">Lorem ipsum dolor sit amet.</h1>
+            <p class="text-description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+              iste dolores nam velit modi distinctio rerum quae libero debitis,
+              tenetur vitae mollitia repudiandae? Iure quo tempora nihil
+              voluptate totam minus.
+            </p>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
